2cc08d12fb499142cc951c121fb64aed7ba1fc14
Kassie Bradshaw <kassiescodes@gmail.com>
2024-01-15 12:12:02 -0800

    Completes challenge 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let stringArray = string.split('');
+  let arrayLength = stringArray.length;
+  let reversedString = '';
+  for (let i = 0; i < arrayLength; i++){
+    reversedString += stringArray.pop();
+  }
+  return reversedString;
 
 };
 
