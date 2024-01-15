9b599050424da62a3c26536b70e22117159cb962
Kassie Bradshaw <kassiescodes@gmail.com>
2024-01-15 13:07:11 -0800

    Completes challenge 05, challenge 04 unfinished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, numToRemove) {
+  let resultArray = [];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] !== numToRemove) {
+      resultArray.push(array[i])
+    }
+  }
+  return resultArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+  // DONE: function requires 2 integers as parameters
+  // DONE: negative numbers will error
+  // TODO: non-numbers will error
+  // DONE: firstNum could be greater than secondNum
+  if (firstNum < 0 || secondNum < 0) {
+    return 'ERROR';
+  };
+
+  if (typeof(arguments[0]) === 'number' && typeof(arguments[1]) === 'number') {
+
+    let finalSum = 0;
+
+    if (firstNum < secondNum) {
+      for (let i = firstNum; i <= secondNum; i++) {
+        finalSum += i;
+      }
+    } else if (firstNum > secondNum) {
+      for (let i = secondNum; i <= firstNum; i++) {
+        finalSum += i;
+      }
+    }
+
+    return finalSum;
+  } else {
+    return 'ERROR';
+  }
 
 };
 
